e530fa0ce0a4129f48277a9518b9f666e7bc2f11
dsaw04 <161405969+dsaw04@users.noreply.github.com>
2024-12-03 14:15:09 +1300

    feat:implemented all CRUD operations

--- server/backend/src/routes/taskRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { create } from "../controllers/taskController.js";
-
-const router = express.Router();
-router.post("/tasks", create);
-
-export default router;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import route from "./backend/src/routes/taskRoutes.js";
+import route from "./routes/taskRoutes.js";
 
 dotenv.config();
 
