234b8c767c8ea8f4afd40b6148601297fbd2d965
dsaw04 <161405969+dsaw04@users.noreply.github.com>
2025-01-06 22:48:01 +1300

    style: changed the appearance of the verify page as well.

--- client/src/pages/VerificationPage.tsx
+++ client/src/pages/VerificationPage.tsx
@@ -4,21 +4,19 @@ import { motion } from "framer-motion";
 import toast from "react-hot-toast";
 import { AuthContext } from "../context/AuthContext";
 import axios from "axios";
+import { Blobs } from "../components/Blobs";
 
 const VerificationPage: React.FC = () => {
   const [code, setCode] = useState<string[]>(["", "", "", "", "", ""]);
   const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
   const navigate = useNavigate();
-  const { verifyEmail, resendVerificationEmail } = useContext(AuthContext); // Add resendVerificationEmail from AuthContext
+  const { verifyEmail, resendVerificationEmail } = useContext(AuthContext);
   const [isLoading, setIsLoading] = useState(false);
-  const [isResending, setIsResending] = useState(false); // Track resend state
+  const [isResending, setIsResending] = useState(false);
 
   const handleChange = (index: number, value: string) => {
     const newCode = [...code];
-
-    if (value.length > 1) {
-      return;
-    }
+    if (value.length > 1) return;
 
     newCode[index] = value;
     setCode(newCode);
@@ -59,7 +57,7 @@ const VerificationPage: React.FC = () => {
     try {
       await verifyEmail(verificationCode);
       toast.success("Email verified successfully");
-      navigate("/login"); // Redirect to login after successful verification
+      navigate("/login");
     } catch (error: unknown) {
       if (error instanceof Error) {
         toast.error(error.message);
@@ -74,7 +72,7 @@ const VerificationPage: React.FC = () => {
   const handleResendCode = async () => {
     setIsResending(true);
     try {
-      await resendVerificationEmail(); // Call the resendVerificationEmail method
+      await resendVerificationEmail();
       toast.success("Verification code resent successfully.");
     } catch (error: unknown) {
       if (axios.isAxiosError(error)) {
@@ -98,60 +96,68 @@ const VerificationPage: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-white">
-      <div className="max-w-md w-full bg-gray-100 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden">
-        <motion.div
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-          className="bg-gray-100 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-2xl p-8 w-full max-w-md"
-        >
-          <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-olive to-forestGreen text-transparent bg-clip-text">
-            Verify Your Email
-          </h2>
-          <p className="text-center text-zinc-900 mb-6">
-            Enter the 6-digit code sent to your email address. Check your spam
-            folder if you can't find it.
-          </p>
+    <div className="relative w-screen h-screen">
+      {/* Blobs Background */}
+      <div className="absolute inset-0 z-1">
+        <Blobs />
+      </div>
 
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <div className="flex justify-between">
-              {code.map((digit, index) => (
-                <input
-                  key={index}
-                  ref={(el) => (inputRefs.current[index] = el)}
-                  type="text"
-                  maxLength={1}
-                  value={digit}
-                  onChange={(e) => handleChange(index, e.target.value)}
-                  onKeyDown={(e) => handleKeyDown(index, e)}
-                  onPaste={handlePaste}
-                  className="w-12 h-12 text-center text-2xl font-bold bg-gray-300 text-zinc-900 border-2 border-gray-300 rounded-lg focus:border-green-500 focus:outline-none"
-                />
-              ))}
-            </div>
-            <motion.button
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-              type="submit"
-              disabled={isLoading || code.some((digit) => !digit)}
-              className="w-full bg-gradient-to-r from-olive to-forestGreen text-white font-bold py-3 px-4 rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 disabled:opacity-50"
-            >
-              {isLoading ? "Verifying..." : "Verify Email"}
-            </motion.button>
-          </form>
+      {/* Verification Card */}
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="max-w-md w-full bg-gray-100 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="bg-gray-100 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-2xl p-8 w-full max-w-md"
+          >
+            <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-secondary to-primary text-transparent bg-clip-text">
+              Verify Your Email
+            </h2>
+            <p className="text-center text-zinc-900 mb-6">
+              Enter the 6-digit code sent to your email address. Check your spam
+              folder if you can't find it.
+            </p>
 
-          <p className="text-center text-zinc-700 mt-4">
-            Didn’t receive a code?{" "}
-            <button
-              onClick={handleResendCode}
-              disabled={isResending}
-              className="text-green-500 hover:underline focus:outline-none disabled:opacity-50"
-            >
-              {isResending ? "Resending..." : "Resend Code"}
-            </button>
-          </p>
-        </motion.div>
+            <form onSubmit={handleSubmit} className="space-y-6">
+              <div className="flex justify-between">
+                {code.map((digit, index) => (
+                  <input
+                    key={index}
+                    ref={(el) => (inputRefs.current[index] = el)}
+                    type="text"
+                    maxLength={1}
+                    value={digit}
+                    onChange={(e) => handleChange(index, e.target.value)}
+                    onKeyDown={(e) => handleKeyDown(index, e)}
+                    onPaste={handlePaste}
+                    className="w-12 h-12 text-center text-2xl font-bold bg-gray-300 text-zinc-900 border-2 border-gray-300 rounded-lg focus:border-green-500 focus:outline-none"
+                  />
+                ))}
+              </div>
+              <motion.button
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+                type="submit"
+                disabled={isLoading || code.some((digit) => !digit)}
+                className="w-full bg-gradient-to-r from-secondary to-primary text-white font-bold py-3 px-4 rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 disabled:opacity-50"
+              >
+                {isLoading ? "Verifying..." : "Verify Email"}
+              </motion.button>
+            </form>
+
+            <p className="text-center text-zinc-700 mt-4">
+              Didn’t receive a code?{" "}
+              <button
+                onClick={handleResendCode}
+                disabled={isResending}
+                className="text-green-500 hover:underline focus:outline-none disabled:opacity-50"
+              >
+                {isResending ? "Resending..." : "Resend Code"}
+              </button>
+            </p>
+          </motion.div>
+        </div>
       </div>
     </div>
   );
